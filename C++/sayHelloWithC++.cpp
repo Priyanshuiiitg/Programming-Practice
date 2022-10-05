8898619a9155ccc4718e06bfda62d9502f4a599d
Priyanshuiiitg <98309398+Priyanshuiiitg@users.noreply.github.com>
2022-10-05 15:07:54 +0530

    Update sayHelloWithC++.cpp

--- C++/sayHelloWithC++.cpp
+++ C++/sayHelloWithC++.cpp
@@ -1,9 +1,13 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    // Create a string variable
-    string s;
-    // Taking input from user
-    cin >> s;
-    cout << "Hello," << " " << s ;
+int main()
+{
+    
+    
+    cout<<"Hello World!"<<endl;
+    
+    
+    
+    return 0;
+    
 }
